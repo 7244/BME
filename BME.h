bb445c3e9c8c404bf47e337a8e6087599a656fe0
7244 <65ad4d@gmail.com>
2025-01-26 22:08:41 +0000

    initial

---
+++ BME.h
@@ -0,0 +1,116 @@
+/* +++ deprecated +++ */
+
+#if defined(BME_set_MutexType)
+  #error BME_set_MutexType is no longer here, it will be picked automaticly. \
+    if you want BME to be implemented without any library, define BME_set_NoLibrary 1. \
+    also define BME_set_Sleep
+#endif
+#if defined(BME_set_Backend)
+  #error BME_set_Backend is no longer used. BME will use __platform defines.
+#endif
+
+/* --- deprecated --- */
+
+#ifndef BME_set_Prefix
+  #error ifndef BME_set_Prefix
+#endif
+
+#ifndef BME_set_AreWeInsideStruct
+  #define BME_set_AreWeInsideStruct 0
+#endif
+
+#ifndef BME_set_Language
+  #if BME_set_AreWeInsideStruct
+    #define BME_set_Language 1
+  #else
+    #define BME_set_Language 0
+  #endif
+#endif
+
+#ifndef BME_set_NoLibrary
+  #if defined(__platform_libc)
+    #define BME_set_NoLibrary 0
+  #else
+    #define BME_set_NoLibrary 1
+  #endif
+#endif
+
+#ifndef BME_set_Sleep
+  #define BME_set_Sleep 1
+#endif
+
+#ifndef BME_set_StructFormat
+  #if BME_set_Language == 0
+    #define BME_set_StructFormat 0
+  #elif BME_set_Language == 1
+    #define BME_set_StructFormat 1
+  #else
+    #error ?
+  #endif
+#endif
+
+#ifndef BME_set_LockValue
+  #define BME_set_LockValue 0
+#endif
+#if BME_set_LockValue != 0 && BME_set_LockValue != 1
+  #error ?
+#endif
+
+#define _BME_P(p) CONCAT3(BME_set_Prefix,_,p)
+
+#if BME_set_StructFormat == 0
+  #define BME_StructBegin(n) typedef struct{
+  #define BME_StructEnd(n) }n;
+#elif BME_set_StructFormat == 1
+  #define BME_StructBegin(n) struct n{
+  #define BME_StructEnd(n) };
+#else
+  #error ?
+#endif
+
+#if BME_set_Language == 0
+  #define _BME_POFTWBIT(p0) _P(p0)
+  #define _BME_SOFTWBIT static
+  #define _BME_PTFF t
+  #define _BME_PTFFC _BME_PTFF,
+  #define _BME_DTFF _P(t) *_BME_PTFF
+  #define _BME_DTFFC _BME_DTFF,
+  #define _BME_GetType _BME_PTFF
+  #define _BME_OCITFFE ,
+#elif BME_set_Language == 1
+  #define _BME_POFTWBIT(p0) p0
+  #define _BME_SOFTWBIT
+  #define _BME_PTFF
+  #define _BME_PTFFC
+  #define _BME_DTFF
+  #define _BME_DTFFC
+  #define _BME_GetType this
+  #define _BME_OCITFFE
+#endif
+
+#include "rest.h"
+
+#undef _BME_POFTWBIT
+#undef _BME_SOFTWBIT
+#undef _BME_PTFF
+#undef _BME_PTFFC
+#undef _BME_DTFF
+#undef _BME_DTFFC
+#undef _BME_GetType
+#undef _BME_OCITFFE
+
+#undef BME_StructEnd
+#undef BME_StructBegin
+
+#undef _BME_P
+
+#ifdef BME_set_Conditional
+  #undef BME_set_Conditional
+#endif
+#undef BME_set_LockValue
+#undef BME_set_StructFormat
+#undef BME_set_Sleep
+#undef BME_set_NoLibrary
+#undef BME_set_Language
+#undef BME_set_AreWeInsideStruct
+#undef BME_set_Prefix
---
+++ rest.h
@@ -0,0 +1,228 @@
+BME_StructBegin(_BME_P(t))
+  #if defined(BME_set_Conditional)
+    #if defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+      pthread_mutex_t mutex;
+      pthread_cond_t cond;
+    #elif defined(__platform_windows) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+      CRITICAL_SECTION mutex;
+      CONDITION_VARIABLE cond;
+    #else
+      #error ?
+    #endif
+  #else
+    #if defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+      pthread_mutex_t mutex;
+    #elif !BME_set_Sleep
+      uint8_t value;
+    #else
+      #error ?
+    #endif
+  #endif
+
+  #if BME_set_Language == 1
+    #if BME_set_Sleep
+      _BME_P(t)(){
+        Open();
+      }
+      ~_BME_P(t)(){
+        Close();
+      }
+    #else
+      _BME_P(t)(){
+        Init();
+      }
+    #endif
+  #endif
+#if BME_set_Language == 0
+  BME_StructEnd(_BME_P(t))
+#endif
+
+_BME_SOFTWBIT
+#if BME_set_LockValue == 0
+  void
+#else
+  bool
+#endif
+_BME_POFTWBIT(Lock)(
+  _BME_DTFF
+){
+  #if defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+    int r = pthread_mutex_lock(&_BME_GetType->mutex);
+    if(r != 0){
+      __abort();
+    }
+    #if BME_set_LockValue == 1
+      return 0;
+    #endif
+  #elif defined(BME_set_Conditional) && defined(__platform_windows) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+    EnterCriticalSection(&_BME_GetType->mutex);
+    #if BME_set_LockValue == 1
+      return 0;
+    #endif
+  #elif !defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+    int r = pthread_mutex_lock(&_BME_GetType->mutex);
+    if(r != 0){
+      __abort();
+    }
+    #if BME_set_LockValue == 1
+      return 0;
+    #endif
+  #elif !defined(BME_set_Conditional) && !BME_set_Sleep
+    while(__sync_lock_test_and_set(&_BME_GetType->value, 1)){
+      #if BME_set_LockValue == 1
+        return 1;
+      #else
+        while(_BME_GetType->value){
+          __builtin_ia32_pause();
+        }
+      #endif
+    }
+    #if BME_set_LockValue == 1
+      return 0;
+    #endif
+  #else
+    #error ?
+  #endif
+}
+
+_BME_SOFTWBIT
+void
+_BME_POFTWBIT(Unlock)(
+  _BME_DTFF
+){
+  #if defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+    int r = pthread_mutex_unlock(&_BME_GetType->mutex);
+    if(r != 0){
+      __abort();
+    }
+  #elif defined(BME_set_Conditional) && defined(__platform_windows) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+    LeaveCriticalSection(&_BME_GetType->mutex);
+  #elif !defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+    int r = pthread_mutex_unlock(&_BME_GetType->mutex);
+    if(r != 0){
+      __abort();
+    }
+  #elif !defined(BME_set_Conditional) && !BME_set_Sleep
+    __sync_lock_release(&_BME_GetType->value);
+  #else
+    #error ?
+  #endif
+}
+
+#ifdef BME_set_Conditional
+  _BME_SOFTWBIT
+  void
+  _BME_POFTWBIT(Wait)(
+    _BME_DTFF
+  ){
+    #if defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+      int r = pthread_cond_wait(&_BME_GetType->cond, &_BME_GetType->mutex);
+      if(r != 0){
+        __abort();
+      }
+    #elif defined(BME_set_Conditional) && defined(__platform_windows) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+      bool r = SleepConditionVariableCS(&_BME_GetType->cond, &_BME_GetType->mutex, INFINITE);
+      if(r == 0){
+        __abort();
+      }
+    #else
+      #error ?
+    #endif
+  }
+
+  _BME_SOFTWBIT
+  void
+  _BME_POFTWBIT(Signal)(
+    _BME_DTFF
+  ){
+    #if defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+      int r = pthread_cond_signal(&_BME_GetType->cond);
+      if(r != 0){
+        __abort();
+      }
+    #elif defined(BME_set_Conditional) && defined(__platform_windows) && defined(__platform_libc) && !BME_set_NoLibrary && BME_set_Sleep
+      WakeConditionVariable(&_BME_GetType->cond);
+    #else
+      #error ?
+    #endif
+  }
+#endif
+
+#if BME_set_Language != 0
+  private:
+#endif
+#if BME_set_Sleep
+  _BME_SOFTWBIT
+  void
+  _BME_POFTWBIT(Open)(
+    _BME_DTFF
+  ){
+    #if defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary
+      int r = pthread_mutex_init(&_BME_GetType->mutex, NULL);
+      if(r != 0){
+        __abort();
+      }
+      r = pthread_cond_init(&_BME_GetType->cond, NULL);
+      if(r != 0){
+        __abort();
+      }
+    #elif defined(BME_set_Conditional) && defined(__platform_windows) && defined(__platform_libc) && !BME_set_NoLibrary
+      InitializeCriticalSection(&_BME_GetType->mutex);
+      InitializeConditionVariable(&_BME_GetType->cond);
+    #elif !defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary
+      int r = pthread_mutex_init(&_BME_GetType->mutex, NULL);
+      if(r != 0){
+        __abort();
+      }
+    #else
+      #error ?
+    #endif
+  }
+
+  _BME_SOFTWBIT
+  void
+  _BME_POFTWBIT(Close)(
+    _BME_DTFF
+  ){
+    #if defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary
+      int r = pthread_mutex_destroy(&_BME_GetType->mutex);
+      if(r != 0){
+        __abort();
+      }
+      r = pthread_cond_destroy(&_BME_GetType->cond);
+      if(r != 0){
+        __abort();
+      }
+    #elif defined(BME_set_Conditional) && defined(__platform_windows) && defined(__platform_libc) && !BME_set_NoLibrary
+      DeleteCriticalSection(&_BME_GetType->mutex);
+    #elif !defined(BME_set_Conditional) && defined(__platform_unix) && defined(__platform_libc) && !BME_set_NoLibrary
+      int r = pthread_mutex_destroy(&_BME_GetType->mutex);
+      if(r != 0){
+        __abort();
+      }
+    #else
+      #error ?
+    #endif
+  }
+#else /* #if BME_set_Sleep */
+  _BME_SOFTWBIT
+  void
+  _BME_POFTWBIT(Init)(
+    _BME_DTFF
+  ){
+    #if !defined(BME_set_Conditional)
+      _BME_GetType->value = 0;
+    #else
+      #error ?
+    #endif
+  }
+#else
+  #error ?
+#endif
+#if BME_set_Language != 0
+  public:
+#endif
+
+#if BME_set_Language != 0
+  BME_StructEnd(_BME_P(t))
+#endif
